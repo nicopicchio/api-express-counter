a88fda3410bf239021d2b476fe7beb94475af468
Nico Picchio <nicolapicchio@gmail.com>
2022-03-21 18:01:04 +0000

    core criteria and extension 1 completed

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
 //Include the express library
-const express = require("express")
+const express = require('express');
 //Include the morgan middleware
-const morgan = require("morgan")
+const morgan = require('morgan');
 //Include the cors middleware
-const cors = require("cors")
+const cors = require('cors');
 //Create a new express application
-const app = express()
+const app = express();
 
 //Tell express we want to use the morgan library
-app.use(morgan("dev"))
+app.use(morgan('dev'));
 //Tell express we want to use the cors library
-app.use(cors())
+app.use(cors());
 
 //Start up our server
-const port = 3030
+const port = 3030;
 app.listen(port, () => {
- console.log(`Server is running on http://localhost:${port}/`)
-})
+	console.log(`Server is running on http://localhost:${port}/`);
+});
+
+let counter = 0;
+
+app.get('/counter', (req, res) => {
+	res.json({ counter: counter });
+});
+
+app.post('/counter/increment', (req, res) => {
+	counter++;
+	res.json({ counter: counter });
+});
+
+app.post('/counter/decrement', (req, res) => {
+	counter--;
+	res.json({ counter: counter });
+});
+
+app.post('/counter/double', (req, res) => {
+	counter *= 2;
+	res.json({ counter: counter });
+});
+
+app.delete('/counter', (req, res) => {
+	counter = 0;
+	res.json({ counter: counter });
+});
+
+app.put('/counter', (req, res) => {
+	if (!req.query.value) {
+		res.json({ counter: counter });
+	}
+	counter = Number(req.query.value);
+	res.json({ counter: counter });
+});
